3abc10482244e56122c54ec498318272721f7dc4
NiccolasBon <agustindenis03@gmail.com>
2024-04-03 10:23:05 -0300

    Initial commit

---
+++ src/main/java/com/app/controller/TestAuthController.java
@@ -0,0 +1,34 @@
+package com.app.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class TestAuthController {
+
+    @GetMapping("/get")
+    public String helloGet() {
+        return "Hello World - GET";
+    }
+
+    @PostMapping("/post")
+    public String helloPost() {
+        return "Hello World - POST";
+    }
+
+    @PutMapping("/put")
+    public String helloPut() {
+        return "Hello World - PUT";
+    }
+
+    @DeleteMapping("/delete")
+    public String helloDelete() {
+        return "Hello World - DELETE";
+    }
+
+    @PatchMapping("/patch")
+    public String helloPatch() {
+        return "Hello World - PATCH";
+    }
+}
---
+++ src/main/java/com/app/persistence/entity/RoleEnum.java
@@ -0,0 +1,10 @@
+package com.app.persistence.entity;
+
+public enum RoleEnum {
+
+    ADMIN,
+    USER,
+    INIVITED,
+    DEVELOPER
+
+}
